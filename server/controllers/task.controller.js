dfd0f14f069eecec14ba319b878b4ff89021f377
chamipon <zkucera14@gmail.com>
2025-02-28 12:55:08 -0500

    added GET /tasks/:id endpoint

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,57 +1,71 @@
 const { MongoClient } = require("mongodb");
-var MONGODB_URI="mongodb://root:example@mongo:27017"
+const ObjectId = require("mongodb").ObjectId;
+var MONGODB_URI = "mongodb://root:example@mongo:27017";
 const client = new MongoClient(MONGODB_URI);
 
-class TaskController{
-    static client;
-    TaskController(mongoClient){
-        client = mongoClient;
-    }
-    static async getAllTasks(req,res){
-        try{
-            await client.connect();
-            const db = client.db("kemi");
-            const collection = db.collection("tasks");
-        
-            const cursor = collection.find();
-            const allTasks = await cursor.toArray();
-            res.status(200).send(allTasks);
-            
-        }
-        catch(err){
-            console.error("Error executing query", err)
-            res.status(500).send(err);
-        }
-        finally{
-            await client.close();
-        }
-    }
-    static async addTask(req,res){
-        var task = req.body;
+class TaskController {
+	static async getAllTasks(req, res) {
+		try {
+			await client.connect();
+			const db = client.db("kemi");
+			const collection = db.collection("tasks");
 
-        if(task){
-            try{
-                await client.connect();
-                const db = client.db("kemi");
-                const collection = db.collection("tasks");
+			const cursor = collection.find();
+			const allTasks = await cursor.toArray();
+			res.status(200).send(allTasks);
+		} catch (err) {
+			console.error("Error executing query", err);
+			res.status(500).send(err);
+		} finally {
+			await client.close();
+		}
+	}
+	static async getTask(req, res) {
+		var id = req.params.id;
+		try {
+			await client.connect();
+			const db = client.db("kemi");
+			const collection = db.collection("tasks");
 
-                const result = await collection.insertOne(task);
-                console.log("Task created: " + result.insertedId)
-                res.status(201).send(result.insertedId);
-            }
-            catch{
-                console.error("Error executing query", err)
-                res.status(500).send(err);
-            }
-            finally{
-                await client.close();
-            }
-        }
-        else{
-            res.status(400).send("Invalid request body");
-            console.error("Invalid request")
-        }
-    }
+			const cursor = collection.find({ _id: new ObjectId(id) });
+			const tasks = await cursor.toArray();
+
+			if (tasks.length == 0) {
+				res.status(404).send("No ingredient found with id " + id);
+				console.log("No ingredient found with id " + id);
+			} else {
+				console.log(tasks[0]);
+				res.status(200).send(tasks[0]);
+			}
+		} catch (err) {
+			console.error("Error executing query", err);
+			res.status(500).send(err);
+		}
+	}
+	static async addTask(req, res) {
+		var task = req.body;
+
+		if (task) {
+			try {
+				await client.connect();
+				const db = client.db("kemi");
+				const collection = db.collection("tasks");
+
+				const result = await collection.insertOne(task);
+				console.log("Task created: " + result.insertedId);
+				res.status(201).send(result.insertedId);
+			} catch {
+				console.error("Error executing query", err);
+				res.status(500).send(err);
+			} finally {
+				await client.close();
+			}
+		} else {
+			res.status(400).send("Invalid request body");
+			console.error("Invalid request");
+		}
+	}
+	static async UpdateTask() {}
 }
 
-module.exports = TaskController
+module.exports = TaskController;
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-
 const TaskController = require("../controllers/task.controller");
 
 router.get("/", TaskController.getAllTasks);
+router.get("/:id", TaskController.getTask);
 router.post("/", TaskController.addTask);
 
-module.exports = router
+module.exports = router;

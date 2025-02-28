5fcf93c2fb467097b877b1f0734232200b706d94
chamipon <zkucera14@gmail.com>
2025-02-27 23:25:32 -0500

    MongoDB and NodeJS connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const API_PORT = process.env.API_PORT || 8888;
+
+var MONGODB_URI="mongodb://root:example@mongo:27017"
+const client = new MongoClient(MONGODB_URI);
 
 app.listen(API_PORT, () => {
     console.log(`Now listening on port ${API_PORT}`)
